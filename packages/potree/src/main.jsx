9a7f0b457ebe99b2bf41aff9d09a44d62b969372
Malik Falana <malikfalana@icloud.com>
2026-02-26 13:24:42 -0500

    Change default basePath to 'cloud-viewer'

--- packages/potree/src/main.jsx
+++ packages/potree/src/main.jsx
@@ -29,7 +29,7 @@ export function HwcPotree({
   mapTilerKey,
   onViewerReady,
   orthoUrl,
-  basePath = ''
+  basePath = 'cloud-viewer'
 }) {
   const potreeContainerRef = useRef(null);
   const cesiumContainerRef = useRef(null);
